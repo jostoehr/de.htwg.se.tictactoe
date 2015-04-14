14dbc4634ba8dce34b0abbfddb01366ad3c80943
Johannes Stoehr <jostoehr@htwg-konstanz.de>
2015-04-14 23:22:15 +0200

    test

--- src/de/htwg/tictactoe/entities/Cell.java
+++ src/de/htwg/tictactoe/entities/Cell.java
@@ -5,17 +5,16 @@ public class Cell {
     private int column;
     private char value;
 
-    public Cell(int row, int column){
+    public Cell(int row, int column) {
         this.row=row;
         this.column=column;
     }
 
-    public void setValue(char value){
+    public void setValue(char value) {
         this.value = value;
     }
 
-    public char getValue()
-    {
+    public char getValue() {
         return value;
     }
 }
