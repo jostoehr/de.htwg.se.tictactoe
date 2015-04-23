981451cebe755a072e98fd4f60a2643a004355b4
Johannes Stoehr <jostoehr@htwg-konstanz.de>
2015-04-23 15:56:06 +0200

    controller with setValue method

--- src/de/htwg/tictactoe/controller/TictactoeController.java
+++ src/de/htwg/tictactoe/controller/TictactoeController.java
@@ -1,9 +1,41 @@
 package de.htwg.tictactoe.controller;
 
+import de.htwg.tictactoe.entities.Cell;
+import de.htwg.tictactoe.entities.Grid;
+import de.htwg.util.observer.Observable;
+
 /**
  *
  * @author siegfried
  */
-public class TictactoeController {
+public class TictactoeController extends Observable {
     
+    private String statusMessage = "Welcome to HTWG TicTacToe!";
+    private Grid grid;
+    
+    public TictactoeController(Grid grid) {
+        this.grid = grid;
+    }
+    
+    public void setValue(int row, int column, char value) {
+	Cell cell = grid.getCell(row, column);
+	if (cell.isUnSet()) {
+		cell.setValue(value);
+		setStatusMessage("The cell " + cell.mkString() + " was successfully set");
+	} else 
+            setStatusMessage("The cell " + cell.mkString() + " is already set");
+	notifyObservers();
+    }
+    
+    public String getStatus() {
+	return statusMessage;
+    }
+    
+    private void setStatusMessage(String statusMessage) {
+	this.statusMessage = statusMessage;
+    }
+    
+    public String getGridString() {
+	return grid.toString();
+    }
 }
