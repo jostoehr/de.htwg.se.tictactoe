47f0557aa86f6b0818fbcfd7ddd7b612f059f71b
sikienzl <sikienzl@htwg-konstanz.de>
2015-04-24 19:04:19 +0200

    convert to maven

---
+++ src/test/java/de/htwg/util/observer/ObservableTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.htwg.util.observer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+/**
+ *
+ * @author siegfried
+ */
+public class ObservableTest {
+    
+    @Before
+    public void setUp() throws Exception {
+        
+    }
+    
+    @Test
+    public void testGetValue(){
+        
+    }
+}
