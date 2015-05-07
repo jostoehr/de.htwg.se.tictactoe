239745acf7e19f1bd8b2888b8d337e6a1c3de9ef
Johannes Stoehr <jostoehr@htwg-konstanz.de>
2015-05-07 17:29:21 +0200

    create setPlayer1 and setPlayer2, remove complex setPlayer

---
+++ src/de/htwg/tictactoe/controller/PlayerController.java
@@ -0,0 +1,30 @@
+package de.htwg.tictactoe.controller;
+
+import de.htwg.tictactoe.entities.Enum;
+import de.htwg.tictactoe.entities.Player;
+
+/**
+ *
+ * @author johannes
+ */
+public class PlayerController {
+    Player player1;
+    Player player2;
+    
+    
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+    
+    public final void setPlayer1(String name, Enum character) {
+        this.player1 = new Player(name, character);
+    }
+
+    public final void setPlayer2(String name, Enum character) {        
+        this.player2 = new Player(name, character);
+    }
+}
