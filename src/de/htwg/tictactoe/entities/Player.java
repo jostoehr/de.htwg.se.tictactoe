0597f109da01fa75785204768fe68a86d8cd6d09
Johannes Stoehr <jostoehr@htwg-konstanz.de>
2015-05-07 13:10:01 +0200

    class player to save an player with name and character

---
+++ src/de/htwg/tictactoe/entities/Player.java
@@ -0,0 +1,33 @@
+package de.htwg.tictactoe.entities;
+
+/**
+ *
+ * @author johannes
+ */
+public class Player {
+    private String name;
+    private Enum character;
+    
+    public Player(String name, Enum character) {
+        this.name = name;
+        this.character = character;
+    }
+    
+    public final void setCharacter(Enum character) {
+        this.character = character;
+    }
+    
+    public Enum getCharacter() {
+        return character;
+    }
+    
+    public final void setName(String name) {
+        if(this.name.isEmpty()) {
+            this.name = name;
+        }
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+}
