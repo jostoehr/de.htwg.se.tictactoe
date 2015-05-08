f8bfbf4549da2c836ad7fedec7c3b6363a38758b
jostoehr <jostoehr@ain-winsrv1.in.fhkn.de>
2015-05-08 12:48:14 +0200

    changed menu of tui and add printHelp() function

--- src/de/htwg/tictactoe/tui/TextUI.java
+++ src/de/htwg/tictactoe/tui/TextUI.java
@@ -1,13 +1,14 @@
 package de.htwg.tictactoe.tui;
 
 import de.htwg.tictactoe.controller.PlayerController;
-import de.htwg.util.observer.IObserver;
 import de.htwg.tictactoe.controller.TictactoeController;
+import de.htwg.tictactoe.entities.Enum;
+import de.htwg.tictactoe.entities.Grid;
+import de.htwg.tictactoe.entities.Player;
+import de.htwg.util.observer.IObserver;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import de.htwg.tictactoe.entities.Enum;
-import de.htwg.tictactoe.entities.Player;
 
 /**
  *
@@ -17,6 +18,7 @@ public class TextUI implements IObserver {
 
     private TictactoeController controller;
     private PlayerController pcontroller;
+    private Grid grid;
     private Player player1;
     private Player player2;
     Scanner scanner;
@@ -28,8 +30,6 @@ public class TextUI implements IObserver {
         player();        
     }
     
-    
-    
     @Override
     public void update() {
         printTUI();
@@ -38,29 +38,34 @@ public class TextUI implements IObserver {
     public void printTUI() {
         System.out.println(controller.getGridString());
 	System.out.println(controller.getStatus());
-
-        System.out
-                .println("Please enter a command( q - quit, n - new, "
-                        + " xyz - set cell(x,y) and z 0 = x or 1 = o)");
+        printHelp();
     }
     
     public boolean processInputLine(String line){
         boolean continu = true;
-        if (line.equalsIgnoreCase("q")) {
-            continu = false;
+        if (line.equalsIgnoreCase("h")) {
+            printHelp();
         } 
         if (line.equalsIgnoreCase("n")) {
-            
+            grid = new Grid();
+            controller = new TictactoeController(grid);
+            controller.addObserver(this);
         }
-        if (line.matches("[0-9][0-9][0-1]")){
-            int[] arg = readToArray(line);
-            Enum value;
-            if(arg[2] == 0){
-                value = Enum.CROSS;
-            } else {
-                value = Enum.NOUGHT;
+        if (line.equalsIgnoreCase("s")) {
+            if (line.matches("[0-9][0-9][ox]")){
+                int[] arg = readToArray(line);
+                Enum value;
+                if(arg[2] == 'x'){
+                    value = Enum.CROSS;
+                } else {
+                    value = Enum.NOUGHT;
+                } 
+                controller.setValue(arg[0], arg[1], value);
             }
-            controller.setValue(arg[0], arg[1], value);
+        }
+        
+        if (line.equalsIgnoreCase("q")) { 
+            continu = false;
         }
         return continu;
     }
@@ -86,5 +91,11 @@ public class TextUI implements IObserver {
         pcontroller.setPlayer2(playername[1], Enum.NOUGHT);
         player1 = pcontroller.getPlayer1();
         player2 = pcontroller.getPlayer2();
-    } 
+    }
+    
+    private void printHelp() {
+        System.out.println("\n-----MENU-----\nh\t-\tHelp\nn\t-\tNew Game\n"
+                + "s\t-\tSet Cell((x,y) = x or 0) like 1 2 x\n"
+                + "q\t-\tquit");
+    }
 }
