7b46ae791b03bfdc65d806d193e6d4a1d6120a8b
Johannes Stoehr <jostoehr@htwg-konstanz.de>
2015-05-13 02:14:22 +0200

    add more output messages, fixed set Cell option bug and add WinController to check if a player just hast won but with bug

--- src/de/htwg/tictactoe/tui/TextUI.java
+++ src/de/htwg/tictactoe/tui/TextUI.java
@@ -2,6 +2,7 @@ package de.htwg.tictactoe.tui;
 
 import de.htwg.tictactoe.controller.PlayerController;
 import de.htwg.tictactoe.controller.TictactoeController;
+import de.htwg.tictactoe.controller.WinController;
 import de.htwg.tictactoe.entities.Enum;
 import de.htwg.tictactoe.entities.Grid;
 import de.htwg.tictactoe.entities.Player;
@@ -18,12 +19,16 @@ public class TextUI implements IObserver {
 
     private TictactoeController controller;
     private PlayerController pcontroller;
+    private WinController wcontroller;
     private Grid grid;
     private Scanner scanner;
+    private Player player1;
+    private Player player2;
     
-    public TextUI(TictactoeController controller, PlayerController pcontroller) {
+    public TextUI(TictactoeController controller, PlayerController pcontroller, WinController wcontroller) {
         this.controller = controller;
         this.pcontroller = pcontroller;
+        this.wcontroller = wcontroller;
         scanner = new Scanner(System.in);
         player();        
     }
@@ -48,18 +53,27 @@ public class TextUI implements IObserver {
             grid = new Grid();
             controller = new TictactoeController(grid);
             controller.addObserver(this);
+            System.out.println("New Game is created");
+            System.out.println(player1.getName() + " it's your turn!");
         }
         if (line.equalsIgnoreCase("s")) {
-            if (line.matches("[0-9][0-9][ox]")){
+            line = scanner.next();
+            if (line.matches("[0-9][0-9][0-1]")){
                 int[] arg = readToArray(line);
                 Enum value;
-                if(arg[2] == 'x'){
+                if(arg[2] == 0) {
                     value = Enum.CROSS;
                 } else {
                     value = Enum.NOUGHT;
                 } 
                 controller.setValue(arg[0], arg[1], value);
             }
+//            if(wcontroller.win().equals(player1))
+//                System.out.println(player1.getCharacter() + " wins");
+//            else if(wcontroller.win().equals(player2))
+//                System.out.println(player2.getCharacter() + " wins");
+//            else if(wcontroller.win().equals(null))
+//                System.out.println("Game is Draw! Press 'n' to restart");
         }
         
         if (line.equalsIgnoreCase("q")) { 
@@ -82,18 +96,22 @@ public class TextUI implements IObserver {
         String playername[] = new String[2];
         for(int i = 0; i < 2; i++) {
             System.out.println("Type in Playername" + (i + 1) + ": ");
-            scanner = new Scanner(System.in);
             playername[i] = scanner.next();
         }
         pcontroller.setPlayer1(playername[0], Enum.CROSS);
         pcontroller.setPlayer2(playername[1], Enum.NOUGHT);
         Player player1 = pcontroller.getPlayer1();
+        this.player1 = player1;
         Player player2 = pcontroller.getPlayer2();
+        this.player2 = player2;
+        //new WinController(grid, player1, player2);
+        System.out.println(player1.getName() + " is " + player1.getCharacter() 
+                + ", " + player2.getName() + " is " + player2.getCharacter());
     }
     
     private void printHelp() {
         System.out.print("\n-----MENU-----\nh\t-\tHelp\nn\t-\tNew Game\n"
-                + "s\t-\tSet Cell((x,y) = x or 0) like 1 2 x\n"
-                + "q\t-\tquit");
+                + "s\t-\tSet Cell((x,y) = 0 for x or 1 for o) like 1 2 0\n"
+                + "q\t-\tquit\n");
     }
 }
