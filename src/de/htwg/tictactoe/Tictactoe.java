b23b33f41f6036352a0d38f328802f21e5c20b01
Johannes Stoehr <jostoehr@htwg-konstanz.de>
2015-05-13 02:10:14 +0200

    add printTUI() in endless loop to update output, add Controller in TUI constructor

--- src/de/htwg/tictactoe/Tictactoe.java
+++ src/de/htwg/tictactoe/Tictactoe.java
@@ -2,6 +2,7 @@ package de.htwg.tictactoe;
 
 import de.htwg.tictactoe.controller.PlayerController;
 import de.htwg.tictactoe.controller.TictactoeController;
+import de.htwg.tictactoe.controller.WinController;
 import de.htwg.tictactoe.entities.Grid;
 import de.htwg.tictactoe.tui.TextUI;
 import java.util.Scanner;
@@ -11,13 +12,14 @@ public final class Tictactoe {
     private static Scanner scanner;
     
     public static void main(final String[] args) {
-        TextUI tui = new TextUI(new TictactoeController(new Grid()), new PlayerController());
+        TextUI tui = new TextUI(new TictactoeController(new Grid()), new PlayerController(), new WinController(new Grid()));
         tui.printTUI();
 
         boolean continu = true;
         scanner = new Scanner(System.in);
         while(continu) {
             continu = tui.processInputLine(scanner.next());
+            tui.printTUI();
         }
     }
 }
