600b1ddcbb0b2de32bcc93f1a04b7f00faa71250
sikienzl <sikienzl@htwg-konstanz.de>
2015-05-16 12:51:54 +0200

    add some instance-variables (WAITING, ATTACK) a constructor and the methods playerItsYourTurn and playerYouAreWaiting. Try to implement the state-pattern

--- src/de/htwg/tictactoe/controller/PlayerController.java
+++ src/de/htwg/tictactoe/controller/PlayerController.java
@@ -10,6 +10,44 @@ import de.htwg.tictactoe.entities.Player;
 public class PlayerController {
     private Player player1;
     private Player player2;
+    /*Zustände für das State-Pattern*/
+    final static int ATTACK = 1;
+    final static int WAITING = 0;
+    
+    /*Instanzvariable 'zustand' zum festhalten
+    * in welchem Zustand wir gerade sind.
+    */
+    int zustand = ATTACK;
+    /*
+    * Mit 'anzahl' wird verfolgt wie viele 
+    * Zeichen, der Spieler noch besitzt
+    */
+    int anzahl = 0;
+    /*Ist 'anzahl' nicht null, wird der Zustand
+    * auf WAITING gesetzt. 
+    */
+    public PlayerController(int anzahl){
+        this.anzahl = anzahl;
+        if (anzahl > 0){
+            zustand = WAITING;
+        }
+    }
+    /*Methode die prüft ob Spieler an der Reihe ist*/
+    public void playerItsYourTurn(){
+        if(zustand == ATTACK){
+            System.out.println("Spieler Sie sind dran!");
+        } else {
+            System.out.println("Spieler Sie sind nicht dran!");
+        }
+    }
+    /*Methode die prüft ob Spieler wartet*/
+    public void playerYouAreWaiting(){
+        if(zustand == WAITING) {
+            System.out.println("Spieler Sie müssen warten!");
+        } else {
+            System.out.println("Spieler Sie müssen nicht warten!");
+        }
+    }
     
     
     public Player getPlayer1() {
