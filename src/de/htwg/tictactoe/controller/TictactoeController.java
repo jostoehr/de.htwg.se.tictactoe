811e2aee66e0e27fcf0040f2469fb35a4bb4bbd9
Johannes Stoehr <jostoehr@htwg-konstanz.de>
2015-05-20 16:08:25 +0200

    add currentState with getter and setter and changed setValue method

--- src/de/htwg/tictactoe/controller/TictactoeController.java
+++ src/de/htwg/tictactoe/controller/TictactoeController.java
@@ -1,4 +1,9 @@
 package de.htwg.tictactoe.controller;
+import de.htwg.tictactoe.controller.impl.StateCrossPlaying;
+import de.htwg.tictactoe.controller.impl.StateCrossWon;
+import de.htwg.tictactoe.controller.impl.StateDraw;
+import de.htwg.tictactoe.controller.impl.StateNoughtPlaying;
+import de.htwg.tictactoe.controller.impl.StateNoughtWon;
 import de.htwg.tictactoe.entities.Cell;
 import de.htwg.tictactoe.entities.Grid;
 import de.htwg.tictactoe.entities.Enum;
@@ -12,20 +17,40 @@ public class TictactoeController extends Observable {
     
     private String statusMessage = "Welcome to HTWG TicTacToe!";
     private final Grid grid;
+    private IGameState currentState;
     
     public TictactoeController(Grid grid) {
         this.grid = grid;
         grid.init();
     }
     
-    public void setValue(int row, int column, Enum value) {
+    public void setValue(int row, int column) {
 	Cell cell = grid.getCell(row, column);
-	if (cell.isUnSet()) {
-		cell.setValue(value);
-		setStatusMessage("The cell " + cell.mkString() + " was successfully set\n");
+	if (cell.isUnSet()) { 
+            if(currentState instanceof StateCrossPlaying) {
+                System.out.println("cross");
+                cell.setValue(Enum.CROSS);
+            }
+            if(currentState instanceof StateNoughtPlaying)
+                cell.setValue(Enum.NOUGHT);
+            setStatusMessage("The cell " + cell.mkString() + " was successfully set\n");
 	} else {
             setStatusMessage("The cell " + cell.mkString() + " is already set\n");
         }
+        getCurrentState().change();
+	notifyObservers();
+    }
+    
+    
+    public void setValue(int row, int column, Enum value) {
+	Cell cell = grid.getCell(row, column);
+	if (cell.isUnSet()) { 
+                cell.setValue(value);
+                setStatusMessage("The cell " + cell.mkString() + " was successfully set\n");
+	} else {
+            setStatusMessage("The cell " + cell.mkString() + " is already set\n");
+        }
+        getCurrentState().change();
 	notifyObservers();
     }
     
@@ -39,5 +64,21 @@ public class TictactoeController extends Observable {
     
     public String getGridString() {
 	return grid.toString();
+    }
+    
+    public void setCurrentState(IGameState currentState) {
+        this.currentState = currentState;
+    }
+    
+    public IGameState getCurrentState() {
+        return currentState;
+    }
+    
+    public void endGame() {
+        if(this.currentState instanceof StateNoughtWon ||
+                this.currentState instanceof StateCrossWon ||
+                this.currentState instanceof StateDraw) {
+            
+        }
     }
 }
