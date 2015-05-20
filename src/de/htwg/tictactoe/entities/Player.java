eddbeba5ecdaa129bf4b5657871e404ed91c1b94
Johannes Stoehr <jostoehr@htwg-konstanz.de>
2015-05-21 00:48:41 +0200

    add draw, win and lost counter with getter and setter vor Statistic

--- src/de/htwg/tictactoe/entities/Player.java
+++ src/de/htwg/tictactoe/entities/Player.java
@@ -7,9 +7,16 @@ package de.htwg.tictactoe.entities;
 public class Player {
     private String name;
     private Enum character = Enum.EMPTY;
+    private int draw = 0;
+    private int win = 0;
+    private int loose = 0;
     
     public Player(String name, Enum character) {
         this.name = name;
+        this.character = character;
+    }
+    
+    public void setCharacter(Enum character) {
         this.character = character;
     }
     
@@ -19,5 +26,28 @@ public class Player {
     
     public String getName() {
         return this.name;
+    }
+    
+    public void setDrawCount(int draw) {
+        this.draw = draw;
+    }
+    
+    public int getDrawCount() {
+        return draw;
+    }
+    
+    public void setWinCount(int win) {
+        this.win = win;
+    }
+    
+    public int getWinCount() {
+        return win;
+    }
+    
+    public void setLooseCount(int loose) {
+        this.loose = loose;
+    }
+    public int getLooseCount() {
+        return loose;
     }
 }
