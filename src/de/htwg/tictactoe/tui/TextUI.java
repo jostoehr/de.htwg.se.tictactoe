aeac1755c8f32fa420eb8c9f7b70d6377d16bd44
Johannes Stoehr <jostoehr@htwg-konstanz.de>
2015-05-20 15:00:25 +0200

    remove line

--- src/de/htwg/tictactoe/tui/TextUI.java
+++ src/de/htwg/tictactoe/tui/TextUI.java
@@ -70,7 +70,6 @@ public class TextUI implements IObserver {
                         value = Enum.NOUGHT;
                     } 
                     controller.setValue(arg[0], arg[1], value);
-                    //wcontroller.winCheck(arg[0], arg[1]);
                 }
             } else {
                 System.out.println("Game is over, press 'n' to restart");
