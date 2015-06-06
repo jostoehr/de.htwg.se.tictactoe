e01b68a6bd18397e0c216e903136b23abd2d56f1
sikienzl <sikienzl@htwg-konstanz.de>
2015-06-06 21:36:17 +0200

    Rename EnumTest to ValueTest

---
+++ test/de/htwg/tictactoe/entities/ValueTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.htwg.tictactoe.entities;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author siegfried
+ */
+public class ValueTest {
+    private Enum cross;
+    private Enum nought;
+    private Enum empty;
+    @Before
+    public void setUp() {
+        cross = Value.CROSS;
+        nought = Value.NOUGHT;
+        empty = Value.EMPTY;
+    }
+    @Test
+    public void TestToString(){
+        assertEquals("x", cross.toString());
+        assertEquals("o", nought.toString());
+        assertEquals(" ", empty.toString());
+        assertEquals(Value.EMPTY, Value.valueOf("EMPTY"));
+        assertEquals(Value.CROSS, Value.valueOf("CROSS"));
+        assertEquals(Value.NOUGHT, Value.valueOf("NOUGHT"));
+    }
+}
