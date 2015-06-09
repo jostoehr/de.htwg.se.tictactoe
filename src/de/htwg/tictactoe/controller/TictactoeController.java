605c1d8ad33cb520afd0e45d0efd8883ee78bbf2
Johannes Stoehr <jostoehr@htwg-konstanz.de>
2015-06-09 23:43:12 +0200

    add getUnSetCell method to make a String list of all UnSet cells for AI to select a random cell, add randInt method to get a random Integer in range [min, max]

--- src/de/htwg/tictactoe/controller/TictactoeController.java
+++ src/de/htwg/tictactoe/controller/TictactoeController.java
@@ -8,6 +8,10 @@ import static de.htwg.tictactoe.entities.Grid.COLS;
 import static de.htwg.tictactoe.entities.Grid.ROWS;
 import de.htwg.tictactoe.entities.Player;
 import de.htwg.util.observer.Observable;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 
 /**
  *
@@ -208,5 +212,42 @@ public class TictactoeController extends Observable {
                 setCurrentState(State.StateDraw);
             }
         }
-    }      
+    }
+    
+    public List<String> getUnSetCells() {
+        List<String> l = new LinkedList<>();
+        for (int row = 0; row < ROWS; ++row) {
+            for (int col = 0; col < COLS; ++col) {
+                if (grid.getCell(row, col).isUnSet()) {
+                    String cell = row+""+col;
+                    l.add(cell);
+                }
+            }
+        }
+        return l;
+    }
+    
+    /**
+     * Quelle: http://stackoverflow.com/questions/363681/generating-random-integers-in-a-range-with-java
+     * Returns a pseudo-random number between min and max, inclusive.
+     * The difference between min and max can be at most
+     * <code>Integer.MAX_VALUE - 1</code>.
+     *
+     * @param min Minimum value
+     * @param max Maximum value.  Must be greater than min.
+     * @return Integer between min and max, inclusive.
+     * @see java.util.Random#nextInt(int)
+     */
+    public static int randInt(int min, int max) {
+
+        // NOTE: Usually this should be a field rather than a method
+        // variable so that it is not re-seeded every call.
+        Random rand = new Random();
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
 }
