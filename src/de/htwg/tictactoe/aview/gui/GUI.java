d15cc91d95dcb4d84a5b06b018d73779fc403fb6
Johannes Stoehr <jostoehr@htwg-konstanz.de>
2015-06-30 23:52:07 +0200

    GUI add statistic

--- src/de/htwg/tictactoe/aview/gui/GUI.java
+++ src/de/htwg/tictactoe/aview/gui/GUI.java
@@ -6,8 +6,11 @@ import de.htwg.tictactoe.util.State;
 import de.htwg.tictactoe.util.Value;
 import de.htwg.util.observer.Event;
 import de.htwg.util.observer.IObserver;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
@@ -16,12 +19,14 @@ import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import static javax.swing.JOptionPane.showMessageDialog;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 
 /**
  *
@@ -32,12 +37,20 @@ public class GUI extends JFrame implements IObserver {
     private final MasterController master;
     
     private static final int WIDTH = 700;
-    private static final int HEIGHT = 700;
+    private static final int HEIGHT = 800;
     private JMenu menu;
     private JMenuItem newGame;
     private JMenuItem modePlayer;
     private JMenuItem change;
     private JMenuItem close;
+    private JLabel player1;
+    private JLabel player2;
+    private JLabel win1;
+    private JLabel lost1;
+    private JLabel draw1;
+    private JLabel win2;
+    private JLabel lost2;
+    private JLabel draw2;
     
     private JButton buttons[][];
     
@@ -63,8 +76,20 @@ public class GUI extends JFrame implements IObserver {
         O = new ImageIcon(this.getClass().getResource("nought.png"));
         iconOkay = new ImageIcon(this.getClass().getResource("okay.png"));
         
+        player1 = new JLabel(master.getPlayer1().getName());
+        player1.setFont(player1.getFont().deriveFont(Font.BOLD));
+        player2 = new JLabel(master.getPlayer2().getName());
+        player2.setFont(player2.getFont().deriveFont(Font.BOLD));
+        win1 = new JLabel("Gewonnen: 0");
+        lost1 = new JLabel("Verloren: 0");
+        draw1 = new JLabel("Unentschieden: 0");
+        win2 = new JLabel("Gewonnen: 0");
+        lost2 = new JLabel("Verloren: 0");
+        draw2 = new JLabel("Unentschieden: 0");
+        
         JPanel panelButtons = new JPanel();
         panelButtons.setLayout(new GridLayout(3,1));
+        //panelButtons.setPreferredSize(new Dimension(700,700));
         buttons = new JButton[3][3];
         for(int i = 0; i < 3; i++) {
             for(int j = 0; j < 3; j++) {
@@ -75,9 +100,21 @@ public class GUI extends JFrame implements IObserver {
             }
         }
         
+        JPanel panelStats = new JPanel();
+        panelStats.setLayout(new GridLayout(4,2));
+        panelStats.add(player1);
+        panelStats.add(player2);
+        panelStats.add(win1);
+        panelStats.add(win2);
+        panelStats.add(lost1);
+        panelStats.add(lost2);
+        panelStats.add(draw1);
+        panelStats.add(draw2);
+        
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(1,1));
-        panel.add(panelButtons);
+        panel.setLayout(new BorderLayout());
+        panel.add(panelButtons, BorderLayout.CENTER);
+        panel.add(panelStats, BorderLayout.SOUTH);
 
         this.add(panel);
         pack();
@@ -89,10 +126,7 @@ public class GUI extends JFrame implements IObserver {
         
         setLocation((tk.getScreenSize().width-this.getWidth())/2,
                     (tk.getScreenSize().height-this.getHeight())/2);
-        
-
     }
-
 
     private void setMenu() {
         menu = new JMenu("Menü");
@@ -127,7 +161,20 @@ public class GUI extends JFrame implements IObserver {
 
     @Override
     public void update(Event e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    }
+    
+    private void updateStatistic() {
+        if(master.getCurrentState() == State.STATECROSSWON || 
+                master.getCurrentState() == State.STATENOUGHTWON ||
+                master.getCurrentState() == State.STATEDRAW) {
+            win1.setText("Gewonnen: " + master.getPlayer1().getWinCount());
+            lost1.setText("Verloren: " + master.getPlayer1().getLostCount());
+            draw1.setText("Unentschieden: " + master.getPlayer1().getDrawCount());
+            win2.setText("Gewonnen: " + master.getPlayer2().getWinCount());
+            lost2.setText("Verloren: " + master.getPlayer2().getLostCount());
+            draw2.setText("Unentschieden: " + master.getPlayer2().getDrawCount());
+        }
     }
 
     private class Buttonlistener implements ActionListener {
@@ -152,9 +199,18 @@ public class GUI extends JFrame implements IObserver {
                         }
                     }
                 }
-                master.change();
+                updateStatistic();
+                if(master.getCurrentState() == State.STATECROSSWON) {
+                    showMessageDialog(null, "X hat gewonnen!", "Spiel vorbei", JOptionPane.INFORMATION_MESSAGE, iconOkay);
+                }
+                if(master.getCurrentState() == State.STATENOUGHTWON) {
+                    showMessageDialog(null, "O hat gewonnen!", "Spiel vorbei", JOptionPane.INFORMATION_MESSAGE, iconOkay);
+                }
+                if(master.getCurrentState() == State.STATEDRAW) {
+                    showMessageDialog(null, "Unentschieden!", "Spiel vorbei", JOptionPane.ERROR_MESSAGE);
+                }
             } else {
-                showMessageDialog(null, "Starten Sie zuerst ein neues Spiel!", "Spielneustart erforderlich!", JOptionPane.ERROR_MESSAGE);
+                showMessageDialog(null, "Spiel zu Ende, startet Sie ein neues Spiel!", "Spielneustart erforderlich!", JOptionPane.ERROR_MESSAGE);
             }
         }
     }
